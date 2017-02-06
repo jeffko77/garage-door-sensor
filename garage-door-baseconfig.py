a4d03ca6df18fa224b3844a4e87be46355b80511
Jeff Korte <jeffko77@gmail.com>
2017-02-06 06:41:21 +0000

    updated with more code to make the functions actually work. To do: create a catalog with the doors.

--- garage-door-baseconfig.py
+++ garage-door-baseconfig.py
@@ -20,12 +20,31 @@ sensor2 = gpiozero.DigitalInputDevice(7,True,None)
 
 
 # Open/Close the Door by cycling the relay
-def moveDoor(doornum)
-    relay+str(doornum).toggle()
+def move_door(doornum):
+    doornum.toggle()
     sleep(.1)
-    relay+str(doornum).toggle()
+    doornum.toggle()
 
 # Check to see if the door is up for too long
-def checkDoor(doornum)
-    door = sensor+str(doornum)
-    
+def check_door(doornum): 
+    if doornum.is_active==False:
+        print("Door has been up for %i seconds" % doornum.inactive_time)
+        if doornum.inactive_time > 3600:
+            message = "Door %s has been up over an hour" % doornum
+        else:
+            return
+    else:
+        print("Door is closed")
+
+check_door(sensor1)
+check_door(sensor2)
+move_door(relay1)
+move_door(relay2)
+
+# Clean up before exiting
+def cleanup():
+    relay1.close()
+    relay2.close()
+    sensor1.close()
+    sensor2.close()
+
